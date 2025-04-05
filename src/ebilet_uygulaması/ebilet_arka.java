3ccef00af0a14600a352b7f3ff2da29e64b4bb79
pomys <pomys@DESKTOP-1N5OHO4>
2025-04-06 02:21:49 +0300

    First Commit

--- src/ebilet_uygulaması/ebilet_arka.java
+++ src/ebilet_uygulaması/ebilet_arka.java
@@ -6,33 +6,28 @@ import java.util.Scanner;
 
 public class ebilet_arka {
 	public static void main(String[] args) {
-				
+
 		Scanner input = new Scanner(System.in);
-	    List<ebilet> eb = new ArrayList<>(); // polymorphism
-	    
+		List<ebilet> eb = new ArrayList<>(); // polymorphism
+
 		int i = 0;
-	    while (true) {
-	    	eb.add(new ebilet());
-	    	eb.get(i).seferSecimi();
-	    	eb.get(i).seferSaati();
-	    	eb.get(i).koltukAlımı();  	
-	    	eb.get(i).biletNoBelirle();
-	    	eb.get(i).odemeMiktari();
-	    	System.out.println(eb.get(i).toString());
-	    	System.out.println("Bilet satın alımınız başarılı bir şekilde tamamlanmıştır."
-	    	+ "\nYeni bir bilet satın alım işlemi için D'ye basınız.\nVeya çıkış için X'e basınız: ");  
-	    	String cikis = input.next().toUpperCase();
-	    	System.out.println("-------------------------------------------------------------");
-	    	if(cikis.equals("X")) { 
-	    		break;
-	    	}
-	    	else
-	    		i++;	
-	    }
-		
+		while (true) {
+			eb.add(new ebilet());
+			eb.get(i).seferSecimi();
+			eb.get(i).seferSaati();
+			eb.get(i).koltukAlımı();
+			eb.get(i).biletNoBelirle();
+			eb.get(i).odemeMiktari();
+			System.out.println(eb.get(i).toString());
+			System.out.println("Bilet satın alımınız başarılı bir şekilde tamamlanmıştır."
+					+ "\nYeni bir bilet satın alım işlemi için D'ye basınız.\nVeya çıkış için X'e basınız: ");
+			String cikis = input.next().toUpperCase();
+			System.out.println("-------------------------------------------------------------");
+			if (cikis.equals("X")) {
+				break;
+			} else
+				i++;
+		}
+
 	}
 }
-
-
-
-
